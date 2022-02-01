fe56c0e224d96dd7ee2ed192f5f617d6ec352491
jeremydane <jeremy_df9@hotmail.com>
2022-02-01 13:53:58 -0500

    flaski tutorial

---
+++ app.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/<some_name>")
+def hello(some_name='person'):
+    return "Hello {0}".format(some_name)
+    # return render_template('hello.html', name=some_name)
+
+# @app.route("/about")
+# def about():
+#     return render_template('about.html')
+
+if __name__ == "__main__":
+    app.run(debug=True, host=os.getenv("IP", '0.0.0.0'), port=int(os.getenv("PORT", 8080)))
